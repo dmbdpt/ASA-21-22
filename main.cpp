6b5a2ffeeca170d641374a25c390f5c1c65c7315
BraveBoii <davidvalente@DESKTOP-611HD22.localdomain>
2021-12-07 15:39:21 +0000

    test

--- main.cpp
+++ main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 
+#define PROBLEM_1 1 
+#define PROBLEM_2 2 
+
+
 int main()
 {
     int i = 0;
+    int problem_n;
     int *list = (int *)malloc(sizeof(int));
-    while (scanf("%i", list[i]))
+
+    scanf("%i", problem_n);
+    while (scanf("%i", list[i++]))
     {
         list = (int *)malloc(sizeof(list) + sizeof(int));
     }
     return 0;
-}w
+
+    switch(problem_n) {
+        case PROBLEM_1:
+            break;
+        case PROBLEM_2:
+            break;
+    }
+}
