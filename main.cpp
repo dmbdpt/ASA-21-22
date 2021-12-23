4b853a2b8b4c0ac1fea79e1896df1480f539a10c
dmbdpt <diogo.dinis@tecnico.ulisboa.pt>
2021-12-23 13:20:45 +0000

    read() updated

--- main.cpp
+++ main.cpp
@@ -1,50 +1,62 @@
 #include <iostream>
-#include <string>
-#include <string.h>
+#include <sstream>
 #include <vector>
 using namespace std;
 
 #define PROBLEM_1 1
-#define PROBLEM_2 2 
+#define PROBLEM_2 2
 
-vector<int> readListInput() {
-    string rawInput;
-    vector<int> nums;
-    getline(cin, rawInput);
-    char *token, *rawInput_aux = strdup(rawInput.c_str());
-    token = strtok(rawInput_aux, " ");
-    while(token) {
-        nums.push_back(stoi(token));
-        token = strtok(NULL, " ");
+
+vector<int> read()
+{
+    vector<int> v;
+    string c;
+    int n;
+
+    getline(cin, c);
+    istringstream input(c);
+    while (input >> n)
+    {
+        v.push_back(n);
     }
-    return nums;
+    return v;
 }
 
-vector<vector<vector<int>>> findLargestSubsequences(vector<int> list, int* maxSize, int* nOfMaxSizeVectors) {
+vector<vector<vector<int>>> findLargestSubsequences(vector<int> list, int *maxSize, int *nOfMaxSizeVectors)
+{
     vector<vector<vector<int>>> maxSubLists(list.size(), vector<vector<int>>(1));
     maxSubLists[0][0].push_back(list[0]);
-    for (int i = 1; i < list.size(); i++) {
-        for (int j = 0; j < i; j++) {
-            if (list[i] > list[j]) {
-                if ((maxSubLists[i][0].size() < (maxSubLists[j][0].size()))) {
+    for (int i = 1; i < list.size(); i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (list[i] > list[j])
+            {
+                if ((maxSubLists[i][0].size() < (maxSubLists[j][0].size())))
+                {
                     maxSubLists[i].clear();
-                    for(vector<int> &jvectors : maxSubLists[j]) {
+                    for (vector<int> &jvectors : maxSubLists[j])
+                    {
                         maxSubLists[i].push_back(jvectors);
                     }
                 }
-                else if((maxSubLists[i][0].size() == (maxSubLists[j][0].size()))) {
+                else if ((maxSubLists[i][0].size() == (maxSubLists[j][0].size())))
+                {
                     maxSubLists[i].insert(maxSubLists[i].begin(), maxSubLists[j].begin(), maxSubLists[j].end());
                 }
             }
         }
-        for(vector<int> &maxSubList : maxSubLists[i]) {
+        for (vector<int> &maxSubList : maxSubLists[i])
+        {
             maxSubList.push_back(list[i]);
         }
-        if(maxSubLists[i][0].size() > *maxSize) {
-                *maxSize = maxSubLists[i][0].size();
-                *nOfMaxSizeVectors = maxSubLists[i].size();
-            }
-        else if(maxSubLists[i][0].size() == *maxSize) {
+        if (maxSubLists[i][0].size() > *maxSize)
+        {
+            *maxSize = maxSubLists[i][0].size();
+            *nOfMaxSizeVectors = maxSubLists[i].size();
+        }
+        else if (maxSubLists[i][0].size() == *maxSize)
+        {
             *nOfMaxSizeVectors += maxSubLists[i].size();
         }
     }
@@ -53,21 +65,25 @@ vector<vector<vector<int>>> findLargestSubsequences(vector<int> list, int* maxSi
 
 int main()
 {
-    int problem_n;
+    int ex;
+    string c;
+    getline(cin, c);
+    ex = stoi(c);
 
-    scanf("%i\n", &problem_n);
-
-    switch(problem_n) {
-        case PROBLEM_1 : {
-            int maxSize = 0, nOfMaxSizeVectors = 0;
-            vector<vector<vector<int>>> maxSubLists = 
-            findLargestSubsequences(readListInput(), &maxSize, &nOfMaxSizeVectors);
-            printf("%i %i\n", maxSize, nOfMaxSizeVectors);
-        } break;
-        case PROBLEM_2 : {
-            
-        }break;
+    switch (ex)
+    {
+    case PROBLEM_1:
+    {
+        int maxSize = 0, nOfMaxSizeVectors = 0;
+        vector<vector<vector<int>>> maxSubLists =
+            findLargestSubsequences(read(), &maxSize, &nOfMaxSizeVectors);
+        printf("%i %i\n", maxSize, nOfMaxSizeVectors);
+    }
+    break;
+    case PROBLEM_2:
+    {
+    }
+    break;
     }
     return 0;
 }
-
