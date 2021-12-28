cf0ef781393894a83c04fa7ba6aedf36604399b9
dmbdpt <diogo.dinis@tecnico.ulisboa.pt>
2021-12-28 16:39:40 +0000

    Random test updated

--- Makefile
+++ Makefile
@@ -10,7 +10,7 @@ rand: random_k.cpp
 	g++ -O3 -Wall -o random_k random_k.cpp -lm
 
 testrand: rand
-	./random_k 1 10 0.999 200 > testfile
+	./random_k 1 10 0.5 200 > testfile
 	./main.o < testfile
 
 clean:
--- random_k.cpp
+++ random_k.cpp
@@ -3,14 +3,15 @@
  * Pedro T. Monteiro
  *************************************************************/
 
-#include<string>
-#include<iostream>
-#include<climits>
+#include <string>
+#include <iostream>
+#include <climits>
 
 using namespace std;
 
 //-------------------------------------------------------------------
-void printUsage() {
+void printUsage()
+{
 	cout << "Sequence generator" << endl;
 	cout << ". sequences of k numbers, with prob p of increasing from i to i+1" << endl;
 	cout << "Usage: random_k p a k k2" << endl;
@@ -23,16 +24,22 @@ void printUsage() {
 }
 
 //-------------------------------------------------------------------
-void genSeq(int n, int init, int a) {
+void genSeq(int n, int init, int a)
+{
 	string s;
 	int next = init;
 	srand(rand());
-	for (int i=1; i<=n; i++) {
-		if (rand() < a) next += (rand() % 5) + 1;
-		else            next -= (rand() %  3) + 1;
-		if (next <= 0) { // guard
-			cout << "ERROR: number <= 0" << endl;
-			exit(1);
+	for (int i = 1; i <= n; i++)
+	{
+		if (rand() < a)
+			next += (rand() % 5) + 1;
+		else
+			next -= (rand() % 3) + 1;
+		if (next <= 0)
+		{ // guard
+			// cout << "ERROR: number <= 0" << endl;
+			// exit(1);
+			next = 1;
 		}
 		s.append(to_string(next) + " ");
 	}
@@ -41,48 +48,61 @@ void genSeq(int n, int init, int a) {
 }
 
 //-------------------------------------------------------------------
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	int p, k, k2, ia, i;
 	float a;
 
-	if (argc < 5) printUsage();
+	if (argc < 5)
+		printUsage();
 	sscanf(argv[1], "%d", &p);
-	if (p != 1 && p != 2) {
+	if (p != 1 && p != 2)
+	{
 		cout << "ERROR: p must be either 1 or 2" << endl;
 		printUsage();
 	}
 	sscanf(argv[2], "%d", &i);
-	if (i < 0) {
+	if (i < 0)
+	{
 		cout << "ERROR: i must be > 0" << endl;
 		printUsage();
 	}
 	sscanf(argv[3], "%f", &a);
-	if (a > 1 || a <= 0) { 
+	if (a > 1 || a <= 0)
+	{
 		cout << "ERROR: a must be 0 < a <= 1" << endl;
 		printUsage();
 	}
-	ia = (int)(a*INT_MAX);
+	ia = (int)(a * INT_MAX);
 	sscanf(argv[4], "%d", &k);
-	if (k < 1) { 
+	if (k < 1)
+	{
 		cout << "ERROR: k must be an int > 0" << endl;
 		printUsage();
 	}
-	if (p == 2) {
-		if (argc == 6) {
+	if (p == 2)
+	{
+		if (argc == 6)
+		{
 			sscanf(argv[5], "%d", &k2);
-			if (k2 < 1) {
+			if (k2 < 1)
+			{
 				cout << "ERROR: k2 must be an int > 0" << endl;
 				printUsage();
 			}
-		} else printUsage();
+		}
+		else
+			printUsage();
 	}
 
 	srand(time(NULL));
 	cout << to_string(p) << endl;
-	if (p == 1) genSeq(k,i, ia);
-	else {
-		genSeq(k,i, ia);
-		genSeq(k2,i, ia);
+	if (p == 1)
+		genSeq(k, i, ia);
+	else
+	{
+		genSeq(k, i, ia);
+		genSeq(k2, i, ia);
 	}
 	return 0;
 }
