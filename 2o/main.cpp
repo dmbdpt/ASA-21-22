d7ed44202437332f6e0d17f68c74f0a7da803e93
dmbdpt <diogo.dinis@tecnico.ulisboa.pt>
2022-01-19 22:24:31 +0000

    troca pai filho

--- 2o/main.cpp
+++ 2o/main.cpp
@@ -4,51 +4,79 @@
 #include <vector>
 using namespace std;
 
-bool is_acyclic(int v, vector<vector<int>> matrix, vector<char> colour) {
-    colour[v-1] = 1;
-    for (int u : matrix[v-1]) {
-        if (colour[u-1] == 0) {
-          if(!is_acyclic(u, matrix, colour)) {
+bool is_acyclic(int v, vector<vector<int>> matrix, vector<char> colour)
+{
+    colour[v - 1] = 1;
+    for (int u : matrix[v - 1])
+    {
+        if (colour[u - 1] == 0)
+        {
+            if (!is_acyclic(u, matrix, colour))
+            {
+                return false;
+            }
+        }
+        else if (colour[u - 1] == 1)
+        {
             return false;
-          }
-        } else if(colour[u-1] == 1) {
-            return false;
-        } else {
-          return true;
+        }
+        else
+        {
+            return true;
         }
     }
-    colour[v-1] = 2;
+    colour[v - 1] = 2;
     return true;
 }
 
-vector<vector<int>>* read(int* v1, int* v2) {
-  int a, b, c, d;
-  if(!scanf("%i %i", v1, v2)) return NULL;
-  if(!scanf("%i %i", &c, &d)) return NULL;
-  static vector<vector<int>> matrix1(c);
-  vector<char> colour(c, 0);
-  for(int i = 0; i < d; i++) {
-    if(!scanf("%i %i", &a, &b)) return NULL;
-    if(matrix1[a-1].size() == 2) return NULL;
-    matrix1[a-1].push_back(b);
-  }
-  for(int i = 1; i <= c; i++) {
-    if(!colour[i-1]) {
-      if(!is_acyclic(i, matrix1, colour)) {
+vector<vector<int>> *read(int *v1, int *v2)
+{
+    int pai, filho, n, m;
+
+    if (!scanf("%i %i", v1, v2))
         return NULL;
-      }
+
+    if (!scanf("%i %i", &n, &m))
+        return NULL;
+
+    static vector<vector<int>> matrix1(n);
+    vector<char> colour(n, 0);
+
+    for (int i = 0; i < m; i++)
+    {
+        if (!scanf("%i %i", &pai, &filho))
+            return NULL;
+
+        if (matrix1[filho - 1].size() == 2)
+            return NULL;
+
+        matrix1[filho - 1].push_back(pai);
     }
-  }
-  return &matrix1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (!colour[i - 1])
+        {
+            if (!is_acyclic(i, matrix1, colour))
+            {
+                return NULL;
+            }
+        }
+    }
+    return &matrix1;
 }
 
-int main() {
-  int *v1 = (int*)malloc(sizeof(int)), *v2 = (int*)malloc(sizeof(int));
-  vector<vector<int>> *tree = read(v1, v2);
-  if(tree) {
-    printf("true\n");
-  }
-  else {
-  }
-  return 0;
+int main()
+{
+    int *v1 = (int *)malloc(sizeof(int)), *v2 = (int *)malloc(sizeof(int));
+    vector<vector<int>> *tree = read(v1, v2);
+    if (tree)
+    {
+        printf("true\n");
+    }
+    else
+    {
+        printf("false\n");
+    }
+    return 0;
 }
