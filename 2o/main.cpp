c5472700f1ae4252ccf0a1a44eafdfccdae67ae6
dmbdpt <diogo.dinis@tecnico.ulisboa.pt>
2022-01-20 16:05:20 +0000

    lca by definition

--- 2o/main.cpp
+++ 2o/main.cpp
@@ -29,6 +29,46 @@ bool is_acyclic(int v, vector<vector<int>> matrix, vector<char> colour)
     return true;
 }
 
+vector<int> ancestor(int v1, vector<vector<int>> tree)
+{
+    vector<int> final;
+    if (!tree[v1 - 1].empty())
+        for (int parent : tree[v1 - 1])
+        {
+            if (parent != 0)
+            {
+                final.push_back(parent);
+                vector<int> parentanc = ancestor(parent, tree);
+                final.insert(final.end(), parentanc.begin(), parentanc.end());
+            }
+        }
+    return final;
+}
+
+vector<int> lca(int v1, int v2, vector<vector<int>> tree)
+{
+    vector<int> ancestor1 = ancestor(v1, tree);
+    ancestor1.push_back(v1);
+    vector<int> ancestor2 = ancestor(v2, tree);
+    ancestor2.push_back(v2);
+    sort(ancestor1.begin(), ancestor1.end());
+    sort(ancestor2.begin(), ancestor2.end());
+    vector<int> final;
+    set_intersection(ancestor1.begin(), ancestor1.end(), ancestor2.begin(), ancestor2.end(), back_inserter(final));
+    for (int i : final)
+    {
+        for (int j : tree[i - 1])
+        {
+            vector<int>::iterator f = find(final.begin(), final.end(), j);
+            if (f != final.end())
+            {
+                final.erase(f);
+            }
+        }
+    }
+    return final;
+}
+
 vector<vector<int>> *read(int *v1, int *v2)
 {
     int pai, filho, n, m;
@@ -68,15 +108,26 @@ vector<vector<int>> *read(int *v1, int *v2)
 
 int main()
 {
-    int *v1 = (int *)malloc(sizeof(int)), *v2 = (int *)malloc(sizeof(int));
-    vector<vector<int>> *tree = read(v1, v2);
+    int v1, v2;
+    vector<vector<int>> *tree = read(&v1, &v2);
     if (tree)
     {
-        printf("true\n");
+        vector<int> final = lca(v1, v2, *tree);
+        if (!final.size())
+        {
+            printf("-\n");
+            return 0;
+        }
+        for (int i : final)
+        {
+            if (i != 0)
+                printf("%d ", i);
+        }
+        printf("\n");
     }
     else
     {
-        printf("false\n");
+        printf("-\n");
     }
     return 0;
 }
