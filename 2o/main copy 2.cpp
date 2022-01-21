776ee433d25b811879686341c8f81cab6513544c
BraveBoii <davidvalente@DESKTOP-611HD22.localdomain>
2022-01-21 22:23:43 +0000

    1600

--- 2o/main copy 2.cpp
+++ 2o/main copy 2.cpp
@@ -1,101 +1,101 @@
 #include <iostream>
 #include <vector>
-#include <list>
 #include <algorithm>
 using namespace std;
 
-bool is_acyclic(int v, vector<vector<int>> matrix, vector<int> colour)
+vector<int> colour, colour_1;
+int v1, v2, n;
+vector<vector<int>> tree, tree_t;
+
+bool is_acyclic(int v)
 {
     colour[v - 1] = 1;
-    for (int u : matrix[v - 1])
+    for (int u : tree_t[v - 1])
     {
         if (colour[u - 1] == 0)
         {
-            if (!is_acyclic(u, matrix, colour))
-            {
+            if(!is_acyclic(u)) 
                 return false;
-            }
         }
         else if (colour[u - 1] == 1)
         {
             return false;
-        }
-        else
-        {
-            return true;
         }
     }
     colour[v - 1] = 2;
     return true;
 }
 
-bool read(int *v1, int *v2, int* n, vector<vector<int>> **matrix_o, vector<vector<int>> **matrix_o_t)
+bool read()
 {
     int pai, filho, m;
 
-    if (!scanf("%i %i", v1, v2))
+    if (!scanf("%i %i", &v1, &v2))
         return false;
 
-    if (!scanf("%i %i", n, &m))
+    if (!scanf("%i %i", &n, &m))
         return false;
 
-    vector<int> colour(*n, 0);
-    static vector<vector<int>> matrix(*n, vector<int>());
-    static vector<vector<int>> matrix_t(*n, vector<int>());
+    colour.resize(n, 0);
+    colour_1.resize(n, 0);
+    tree.resize(n);
+    tree_t.resize(n);
 
     for (int i = 0; i < m; i++)
     {
         if (!scanf("%i %i", &pai, &filho))
             return false;
 
-        if (matrix[filho - 1].size() == 2)
+        if (tree[filho - 1].size() == 2)
             return false;
 
-        matrix[filho - 1].push_back(pai);
-        matrix_t[pai - 1].push_back(filho);
+        tree[filho - 1].push_back(pai);
+        tree_t[pai - 1].push_back(filho);
     }
 
-    for (int i = 1; i <= *n; i++)
+    for (int i = 1; i <= n; i++)
     {
         if (!colour[i - 1])
         {
-            if (!is_acyclic(i, matrix, colour))
+            if (!is_acyclic(i))
             {
                 return false;
             }
         }
     }
-    *matrix_o = &matrix;
-    *matrix_o_t = &matrix_t;
     return true;
 }
 
-void colour1_dfs(vector<vector<int>> tree, vector<int> *colour, int v) {
-    (*colour)[v-1] = 1;
+void colour1_dfs(int v) {
+    colour_1[v-1] = 1;
     for(int i : tree[v-1]) {
-        colour1_dfs(tree, colour, i);
+        if(colour_1[i-1] != 1)
+            colour1_dfs(i);
     }
 }
 
-void colour2_dfs(vector<vector<int>> tree, vector<int> *colour, int v) {
-    if((*colour)[v-1]) {
-        (*colour)[v-1] = 2;
+void colour2_dfs(int v) {
+    if(colour_1[v-1]) {
+        colour_1[v-1] = 2;
+    } else {
+        colour_1[v-1] = 3;
     }
     for(int i : tree[v-1]) {
-        colour2_dfs(tree, colour, i);
+        if(colour_1[i-1] < 2) 
+            colour2_dfs(i);
     }
 }
 
-vector<int> lca(int v1, int v2, int n, vector<vector<int>> tree, vector<vector<int>> tree_t) 
+vector<int> lca() 
 {
-    vector<int> colour(n, 0), found;
-    colour1_dfs(tree, &colour, v1);
-    colour2_dfs(tree, &colour, v2);
+    vector<int> found;
+    colour1_dfs(v1);
+    colour2_dfs(v2);
     for(int i = 1; i <= n; i++) {
-        if(colour[i-1] == 2) {
+        if(colour_1[i-1] == 2) {
             found.push_back(i);
             for(int child : tree_t[i-1]) {
-                if(colour[child - 1] == 2) {
+                if(colour_1[child - 1] == 2) {
                     found.pop_back();
                     break;
                 }
@@ -107,12 +107,9 @@ vector<int> lca(int v1, int v2, int n, vector<vector<int>> tree, vector<vector<i
 
 int main()
 {
-    int v1, v2, n;
-    vector<vector<int>> *tree = NULL, *tree_t = NULL;
-    read(&v1, &v2, &n, &tree, &tree_t);
-    if (tree)
+    if (read())
     {
-        vector<int> found = lca(v1, v2, n, *tree, *tree_t);
+        vector<int> found = lca();
         if(!found.size()) {
             printf("-\n");
         }
